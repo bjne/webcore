85494620932d67f611d5660e3602ef38a97712a3
Bjørnar Ness <bjornar.ness@gmail.com>
2018-10-04 21:13:51 +0200

    wrap.lua: remove need_config

--- lib/webcore/hook/wrap.lua
+++ lib/webcore/hook/wrap.lua
@@ -21,29 +21,13 @@ return function(hook, sorted_plugins, hook_plugins)
             local action = plug.action
             local next_wrapper, wrapper = run
 
-            if plug.need_config then
-                wrapper = function(config, ...)
-                    -- print("RUNNING:", hook_name, name)
-                    if config[name] then
-                        local ok, done = action(config[name], ...)
-                        if not ok or done then
-                            return ok, done
-                        end
-                    else
-                        log.CRIT("NO CONFIG FOR:", hook_name, name)
-                    end
-
-                    return next_wrapper(config, ...)
+            wrapper = function(config, ...)
+                local ok, done = action(...)
+                if not ok or done then
+                    return ok, done
                 end
-            else
-                wrapper = function(config, ...)
-                    local ok, done = action(...)
-                    if not ok or done then
-                        return ok, done
-                    end
 
-                    return next_wrapper(config, ...)
-                end
+                return next_wrapper(config, ...)
             end
 
             run = wrapper
